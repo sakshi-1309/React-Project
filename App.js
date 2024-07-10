23b5c71feab215531856499cfcc65dfcd668ff96
sakshi-1309 <sakshigupta1309@gmail.com>
2024-07-10 20:47:46 +0530

    Episode-2

--- App.js
+++ App.js
@@ -16,10 +16,24 @@
  * 
  */
 
-const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1" }, [React.createElement("h1", {}, "I'm an H1 tag"), React.createElement("h2", {}, "I'm an H2 tag")]), React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "I'm an H1 tag"), React.createElement("h2", {}, "I'm an H2 tag")])])
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement("h1",
-    {
+const parent = React.createElement("div", {
+    id: "parent"
+}, [React.createElement("div", {
+        id: "child1"
+    }, [React.createElement("h1", {}, "I'm an H1 tag"),
+        React.createElement("h2", {}, "I'm an H2 tag")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [React.createElement("h1", {}, "I'm an H1 tag"),
+        React.createElement("h2", {}, "I'm an H2 tag")
+    ])
+])
+
+const heading = React.createElement("h1", {
         id: 'heading',
         xyz: 'abc'
     },
